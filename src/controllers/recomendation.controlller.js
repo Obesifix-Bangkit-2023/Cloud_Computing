051cf07257e2cd4f8bf2a78b13c398f6dfdd9944
midoon <harunkoding@gmail.com>
2023-05-26 22:08:38 +0700

    update config file

--- src/controllers/recomendation.controlller.js
+++ src/controllers/recomendation.controlller.js
@@ -1,4 +1,8 @@
-const getFoodRecomendation = (req, res) => {
+const db = require("../config/db");
+
+const getFoodRecomendation = async (req, res) => {
+  const userId = req.params.userId;
+
   return res.json({ message: "Hello Recomendation " + req.params.userId });
 };
 
