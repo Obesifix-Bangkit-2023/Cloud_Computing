d22019208796b5f1ba46686dba3f01432b985568
midoon <harunkoding@gmail.com>
2023-06-10 22:59:35 +0700

    refactoring

--- src/helper/stringSelector.js
+++ src/helper/stringSelector.js
@@ -1,39 +1,35 @@
+const filterStr1 = (str) => {
+  let tempStr = str;
+  const newTempStr_1 = tempStr.replace("c", "");
+  const newTempStr_2 = newTempStr_1.replace("(", "");
+  const newTempStr_3 = newTempStr_2.replace(")", "");
+  const tempRegex = new RegExp('"', "g");
+  const finalTempStr_1 = newTempStr_3.replace(tempRegex, "");
+  return finalTempStr_1;
+};
+
+const filterStr2 = (str) => {
+  let tempStr = str;
+  const tempRegex = new RegExp('"', "g");
+  const finalTempStr_1 = tempStr.replace(tempRegex, "");
+  return finalTempStr_1;
+};
+
 const stringSelector = (payloads) => {
   payloads.forEach((food, i) => {
     // FILTERING KEYWORD
     if (food.keyword[0] === "c") {
-      let tempStr = food.keyword;
-      const newTempStr_1 = tempStr.replace("c", "");
-      const newTempStr_2 = newTempStr_1.replace("(", "");
-      const newTempStr_3 = newTempStr_2.replace(")", "");
-      const tempRegex = new RegExp('"', "g");
-      const finalTempStr_1 = newTempStr_3.replace(tempRegex, "");
-
-      food.keyword = finalTempStr_1;
+      food.keyword = filterStr1(food.keyword);
     } else {
-      let tempStr = food.keyword;
-      const tempRegex = new RegExp('"', "g");
-      const finalTempStr_1 = tempStr.replace(tempRegex, "");
-      food.keyword = finalTempStr_1;
+      food.keyword = filterStr2(food.keyword);
     }
-
     // FILTERING IMAGE
     if (food.image[0] === "c") {
-      let tempStr = food.image;
-      const newTempStr_1 = tempStr.replace("c", "");
-      const newTempStr_2 = newTempStr_1.replace("(", "");
-      const newTempStr_3 = newTempStr_2.replace(")", "");
-      const tempRegex = new RegExp('"', "g");
-      const finalTempStr_1 = newTempStr_3.replace(tempRegex, "");
-
+      const finalTempStr_1 = filterStr1(food.image);
       const arrStr = finalTempStr_1.split(", ");
-
       food.image = arrStr[0];
     } else {
-      let tempStr = food.image;
-      const tempRegex = new RegExp('"', "g");
-      const finalTempStr_1 = tempStr.replace(tempRegex, "");
-      food.image = finalTempStr_1;
+      food.image = filterStr2(food.image);
     }
   });
 };
